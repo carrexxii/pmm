66c96e35354394f220b6b2ebcbef74bdaa8b6098
carrexxii <cjbarrett101@gmail.com>
2024-05-27 22:48:00 +0200

    Rendering video

--- src/main.nim
+++ src/main.nim
@@ -1,22 +1,61 @@
 import
     nsdl,
-    mpv
+    common, mpv
+from std/sugar import `=>`
 
-const
-    WinW = 1280
-    WinH = 800
+var
+    winw = 1280
+    winh = 800
 
 nsdl.init(Video or Events, should_init_ttf = true)
-let (window, renderer) = create_window_and_renderer("PMM", WinW, WinH)
-
+let (window, renderer) = create_window_and_renderer("PMM", winw, winh, Resizeable)
+renderer.set_vsync 0
 renderer.set_draw_colour Grey
 
-var running = true
+let mpv_ctx = mpv.create()
+mpv_ctx.set_property("vo", "libmpv")
+init mpv_ctx
+
+let
+    one = cint 1
+    gl_init_params = OpenGLInitParamsObj(get_proc_address: (ctx: pointer, name: cstring) => gl_get_proc_address name)
+    render_params = [
+        render_param(APIType, cstring "opengl"),
+        render_param(OpenGLInitParams, gl_init_params.addr),
+        render_param(AdvancedControl, one.addr),
+        render_param(Invalid, nil),
+    ]
+
+let mpv_gl = mpv_ctx.create_render_context render_params
+
+let
+    wakeup_on_mpv_render_update = register_event()
+    wakeup_on_mpv_events        = register_event()
+
+mpv_ctx.set_wakeup_cb (ctx: pointer) => push_event Event(kind: nsdl.EventKind wakeup_on_mpv_events)
+
+let cmd = [cstring "loadfile", cstring "test.mp4", nil]
+mpv_ctx.command_async("loadfile", "test.mp4")
+mpv_gl.set_update_cb (ctx: pointer) => push_event Event(kind: nsdl.EventKind wakeup_on_mpv_render_update)
+
+echo &"Initialized SDL ({nsdl.get_version()}) and MPV ({mpv.get_version()})"
+echo &"\tMPV Client -> {mpv_ctx.client_name} ({mpv_ctx.client_id})"
+
+var
+    bufferw = 960
+    bufferh = 640
+    buffer = renderer.create_texture(bufferw, bufferh, access = Target)
+let buffer_fbo = 2'i32 # IDK how to get this (tried texture properties)
+
+var need_redraw = true
+var running     = true
 while running:
     for event in get_events():
         case event.kind
         of Quit:
             running = false
+        of WindowExposed:
+            need_redraw = true
         of Keydown:
             case event.key.keysym.sym
             of KeyEscape:
@@ -24,10 +63,44 @@ while running:
             else:
                 discard
         else:
-            discard
+            if event.kind == nsdl.EventKind wakeup_on_mpv_render_update:
+                let flags = update mpv_gl
+                if flags == Frame:
+                    need_redraw = true
+            elif event.kind == nsdl.EventKind wakeup_on_mpv_events:
+                for mpv_event in mpv_ctx.get_events():
+                    case mpv_event.kind
+                    of LogMessage:
+                        let log_obj = cast[ptr EventLogMessageObj](mpv_event.data)[]
+                        echo log_obj
+                    else:
+                        echo mpv_event.kind
 
-    fill renderer
-    present renderer
+    if need_redraw:
+        need_redraw = false
+        let
+            one = cint 1
+            fbo_obj = opengl_fbo(bufferw, bufferh, fbo = buffer_fbo)
+            params = [
+                render_param(OpenGLFBO, fbo_obj.addr),
+                render_param(Invalid, nil),
+            ]
 
+        renderer.set_target buffer
+        mpv_gl.render params
+        flush renderer
+
+        renderer.reset_target
+
+        renderer.draw_texture(buffer, dst_rect = frect(200, 200, bufferw, bufferh))
+
+        renderer.set_draw_colour Blue
+        renderer.draw_fill_rect frect(100, 100, 200, 200)
+
+        report_swap mpv_gl
+        present renderer
+
+destroy mpv_gl
+destroy mpv_ctx
 destroy window
 nsdl.quit()
--- src/mpv.nim
+++ src/mpv.nim
@@ -1,6 +1,9 @@
-from std/sugar import `->`
+{.push raises: [].}
 
-type CStringArray* = ptr UncheckedArray[cstring]
+from std/strformat import `&`
+from std/sugar     import `->`
+
+type MPVError* = object of CatchableError
 
 func make_version*(major, minor: int): int =
     (major shl 16) or minor
@@ -52,7 +55,7 @@ type
         NodeMap
         ByteArray
 
-    EventID* {.size: sizeof(cint).} = enum
+    EventKind* {.size: sizeof(cint).} = enum
         None             = 0
         Shutdown         = 1
         LogMessage       = 2
@@ -78,6 +81,14 @@ type
         Error    = 4
         Redirect = 5
 
+func `$`*(kind: EventKind): string =
+    let n = int kind
+    if (n >= 0  and n <= 8) or
+       (n >= 16 and n <= 25) and n != 23:
+        repr kind
+    else:
+        "Unknown: " & $n
+
 type
     Handle* = distinct pointer
 
@@ -96,7 +107,7 @@ type
     NodeListObj* = object
         count* : int32
         values*: ptr NodeObj
-        keys*  : CStringArray
+        keys*  : cStringArray
 
     ByteArrayObj* = object
         data*: pointer
@@ -125,7 +136,7 @@ type
 
     EventClientMessageObj* = object
         arg_count*: int32
-        args*     : CStringArray
+        args*     : cStringArray
 
     EventHookObj* = object
         name*: cstring
@@ -135,68 +146,237 @@ type
         result: NodeObj
 
     EventObj* = object
-        id*            : EventID
+        kind*          : EventKind
         error*         : ErrorCode
         reply_userdata*: uint64
         data*          : pointer
 
+# Rendering
+type
+    RenderContext*  = distinct pointer
+    RenderUpdateFn* = (pointer -> void)
+
+    RenderParamKind* {.size: sizeof(cint).} = enum
+        Invalid
+        APIType
+        OpenGLInitParams
+        OpenGLFBO
+        FlipY
+        Depth
+        ICCProfile
+        AmbientLight
+        X11Display
+        WLDisplay
+        AdvancedControl
+        NextFrameInfo
+        BlockForTargetTime
+        SkipRendering
+        DRMDisplay
+        DRMDrawSurfaceSize
+        DRMDisplayV2
+        SWSize
+        SWFormat
+        SWStride
+        SWPointer
+
+    RenderParam* = object
+        kind*: RenderParamKind
+        data*: pointer
+
+    RenderFrameInfoFlag* {.size: sizeof(uint64).} = enum
+        Present    = 1 shl 0
+        Redraw     = 1 shl 1
+        Repeat     = 1 shl 2
+        BlockVSync = 1 shl 3
+
+    RenderFrameInfo* = object
+        flags*      : RenderFrameInfoFlag
+        target_time*: int64
+
+    RenderUpdateFlag* {.size: sizeof(cint).} = enum
+        None  = 0
+        Frame = 1 shr 0
+
+    OpenGLInitParamsObj* = object
+        get_proc_address*    : (ctx: pointer, name: cstring) -> pointer
+        get_proc_address_ctx*: pointer
+
+    OpenGLFBOObj* = object
+        fbo*         : int32
+        w*, h*       : int32
+        internal_fmt*: int32
+
+    OpenGLDRMParamsObj* = object
+        fd*                : int32
+        crtc_id*           : int32
+        connector_id*      : int32
+        atomic_request_ptr*: ptr pointer # TODO: struct _drmModeAtomicReq **atomic_request_ptr;
+        render_fd*         : int32
+
+    OpenGLDRMParamsV2Obj* = object
+        fd*                : int32
+        crtc_id*           : int32
+        connector_id*      : int32
+        atomic_request_ptr*: ptr pointer # TODO: struct _drmModeAtomicReq **atomic_request_ptr;
+        render_fd*         : int32
+
+    OpenGLDRMDrawSurfaceSizeObj* = object
+        width* : int32
+        height*: int32
+
+func render_param*(kind: RenderParamKind; data: pointer | ptr OpenGLInitParamsObj | ptr OpenGLFBOObj | cstring | ptr cint): RenderParam =
+    result.kind = kind
+    result.data = cast[pointer](data)
+
+func opengl_fbo*(w, h: int; fbo = 0'i32; fmt = 0'i32): OpenGLFBOObj =
+    OpenGLFBOObj(w: int32 w, h: int32 h, fbo: fbo, internal_fmt: fmt)
+
 #[ -------------------------------------------------------------------- ]#
 
 using
-    ctx   : Handle
-    udata : uint64
-    name  : cstring
-    format: Format
-    data  : pointer
-    nodep : ptr NodeObj
+    ctx    : Handle
+    ren_ctx: RenderContext
+    udata  : uint64
+    name   : cstring
+    format : Format
+    data   : pointer
+    nodep  : ptr NodeObj
 
 {.push dynlib: "".}
-proc client_api_version*(): culong                    {.importc: "mpv_client_api_version".}
-proc error_string*(error: cint): cstring              {.importc: "mpv_error_string"      .}
-proc free*(data: pointer)                             {.importc: "mpv_free"              .}
-proc client_name*(ctx): cstring                       {.importc: "mpv_client_name"       .}
-proc client_id*(ctx): int64                           {.importc: "mpv_client_id"         .}
-proc create*(): Handle                                {.importc: "mpv_create"            .}
-proc initialize*(ctx): cint                           {.importc: "mpv_initialize"        .}
-proc destroy*(ctx): cint                              {.importc: "mpv_destroy"           .}
-proc terminate_destroy*(ctx)                          {.importc: "mpv_terminate_destroy" .}
-proc create_client*(ctx; name): Handle                {.importc: "mpv_create_client"     .}
-proc create_weak_client*(ctx; name): Handle           {.importc: "mpv_weak_create_client".}
-proc load_config_file*(ctx; file_name: cstring): cint {.importc: "mpv_load_config_file"  .}
-proc free_node_contents*(node: ptr NodeObj)           {.importc: "mpv_free_node_contents".}
+proc mpv_client_api_version*(): culong                         {.importc: "mpv_client_api_version".}
+proc mpv_error_string*(error: ErrorCode): cstring              {.importc: "mpv_error_string"      .}
+proc mpv_free*(data: pointer)                                  {.importc: "mpv_free"              .}
+proc mpv_client_name*(ctx): cstring                            {.importc: "mpv_client_name"       .}
+proc mpv_client_id*(ctx): int64                                {.importc: "mpv_client_id"         .}
+proc mpv_create*(): Handle                                     {.importc: "mpv_create"            .}
+proc mpv_initialize*(ctx): ErrorCode                           {.importc: "mpv_initialize"        .}
+proc mpv_destroy*(ctx): ErrorCode                              {.importc: "mpv_destroy"           .}
+proc mpv_terminate_destroy*(ctx)                               {.importc: "mpv_terminate_destroy" .}
+proc mpv_create_client*(ctx; name): Handle                     {.importc: "mpv_create_client"     .}
+proc mpv_create_weak_client*(ctx; name): Handle                {.importc: "mpv_weak_create_client".}
+proc mpv_load_config_file*(ctx; file_name: cstring): ErrorCode {.importc: "mpv_load_config_file"  .}
+proc mpv_free_node_contents*(node: ptr NodeObj)                {.importc: "mpv_free_node_contents".}
 
-proc command*(ctx; args: CStringArray): cint                          {.importc: "mpv_command"            .}
-proc command_node*(ctx; args, result: ptr NodeObj): cint              {.importc: "mpv_command_node"       .}
-proc command_ret*(ctx; args: CStringArray; result: ptr NodeObj): cint {.importc: "mpv_command_ret"        .}
-proc command_string*(ctx; args: cstring): cint                        {.importc: "mpv_command_string"     .}
-proc command_async*(ctx; udata; args: CStringArray): cint             {.importc: "mpv_command_async"      .}
-proc command_node_async*(ctx; udata; args: ptr NodeObj): cint         {.importc: "mpv_command_node_async" .}
-proc abort_async_command*(ctx; udata)                                 {.importc: "mpv_abort_async_command".}
+proc mpv_command*(ctx; args: cStringArray): ErrorCode                          {.importc: "mpv_command"            .}
+proc mpv_command_node*(ctx; args, result: ptr NodeObj): ErrorCode              {.importc: "mpv_command_node"       .}
+proc mpv_command_ret*(ctx; args: cStringArray; result: ptr NodeObj): ErrorCode {.importc: "mpv_command_ret"        .}
+proc mpv_command_string*(ctx; args: cstring): ErrorCode                        {.importc: "mpv_command_string"     .}
+proc mpv_command_async*(ctx; udata; args: cStringArray): ErrorCode             {.importc: "mpv_command_async"      .}
+proc mpv_command_node_async*(ctx; udata; args: ptr NodeObj): ErrorCode         {.importc: "mpv_command_node_async" .}
+proc mpv_abort_async_command*(ctx; udata)                                      {.importc: "mpv_abort_async_command".}
 
-proc get_time_us*(ctx): int64                            {.importc: "mpv_get_time_us"            .}
-proc get_time_ns*(ctx): int64                            {.importc: "mpv_get_time_ns"            .}
-proc get_property*(ctx; name; format; data): cint        {.importc: "mpv_get_property"           .}
-proc get_property_string*(ctx; name): cstring            {.importc: "mpv_get_property_string"    .}
-proc get_property_osd_string*(ctx; name): cstring        {.importc: "mpv_get_property_osd_string".}
-proc get_property_async*(ctx; udata; name; format): cint {.importc: "mpv_get_property_async"     .}
-proc del_property*(ctx; name): cint                      {.importc: "mpv_del_property"           .}
-proc unobserve_property*(ctx; udata): cint               {.importc: "mpv_unobserve_property"     .}
+proc mpv_get_time_us*(ctx): int64                                 {.importc: "mpv_get_time_us"            .}
+proc mpv_get_time_ns*(ctx): int64                                 {.importc: "mpv_get_time_ns"            .}
+proc mpv_get_property*(ctx; name; format; data): ErrorCode        {.importc: "mpv_get_property"           .}
+proc mpv_get_property_string*(ctx; name): cstring                 {.importc: "mpv_get_property_string"    .}
+proc mpv_get_property_osd_string*(ctx; name): cstring             {.importc: "mpv_get_property_osd_string".}
+proc mpv_get_property_async*(ctx; udata; name; format): ErrorCode {.importc: "mpv_get_property_async"     .}
+proc mpv_del_property*(ctx; name): ErrorCode                      {.importc: "mpv_del_property"           .}
+proc mpv_unobserve_property*(ctx; udata): ErrorCode               {.importc: "mpv_unobserve_property"     .}
 
-proc set_option*(ctx; name; format; data): cint                {.importc: "mpv_set_option"         .}
-proc set_option_string*(ctx; name; data: cstring): cint        {.importc: "mpv_set_option_string"  .}
-proc set_property*(ctx; name; format; data): cint              {.importc: "mpv_set_property"       .}
-proc set_property_string*(ctx; name; data: cstring): cint      {.importc: "mpv_set_property_string".}
-proc set_property_async*(ctx; udata; name; format; data): cint {.importc: "mpv_set_property_async" .}
-proc observe_property*(ctx; udata; name; format): cint         {.importc: "mpv_observe_property"   .}
+proc mpv_set_option*(ctx; name; format; data): ErrorCode                {.importc: "mpv_set_option"         .}
+proc mpv_set_option_string*(ctx; name; data: cstring): ErrorCode        {.importc: "mpv_set_option_string"  .}
+proc mpv_set_property*(ctx; name; format; data): ErrorCode              {.importc: "mpv_set_property"       .}
+proc mpv_set_property_string*(ctx; name; data: cstring): ErrorCode      {.importc: "mpv_set_property_string".}
+proc mpv_set_property_async*(ctx; udata; name; format; data): ErrorCode {.importc: "mpv_set_property_async" .}
+proc mpv_observe_property*(ctx; udata; name; format): ErrorCode         {.importc: "mpv_observe_property"   .}
 
-proc event_name*(id: EventID): cstring                         {.importc: "mpv_event_name"          .}
-proc event_to_node*(dst: ptr NodeObj; src: ptr EventObj): cint {.importc: "mpv_event_to_node"       .}
-proc request_event*(ctx; eid: EventID; enable: cint): cint     {.importc: "mpv_request_event"       .}
-proc request_log_messages*(ctx; min_lvl: cstring): cint        {.importc: "mpv_request_log_messages".}
-proc wait_event*(ctx; timeout: cdouble): ptr EventObj          {.importc: "mpv_wait_event"          .}
-proc wakeup*(ctx)                                              {.importc: "mpv_wakeup"              .}
-proc set_wakeup_callback*(ctx; cb: (pointer -> void); data)    {.importc: "mpv_set_wakeup_callback" .}
-proc wait_async_requests*(ctx)                                 {.importc: "mpv_wait_async_requests" .}
-proc hook_add*(ctx; udata; name; priority: cint): cint         {.importc: "mpv_hook_add"            .}
-proc hook_continue*(ctx; id: uint64): cint                     {.importc: "mpv_hook_continue"       .}
+proc mpv_event_name*(id: EventKind): cstring                            {.importc: "mpv_event_name"          .}
+proc mpv_event_to_node*(dst: ptr NodeObj; src: ptr EventObj): ErrorCode {.importc: "mpv_event_to_node"       .}
+proc mpv_request_event*(ctx; eid: EventKind; enable: cint): ErrorCode   {.importc: "mpv_request_event"       .}
+proc mpv_request_log_messages*(ctx; min_lvl: cstring): ErrorCode        {.importc: "mpv_request_log_messages".}
+proc mpv_wait_event*(ctx; timeout: cdouble): ptr EventObj               {.importc: "mpv_wait_event"          .}
+proc mpv_wakeup*(ctx)                                                   {.importc: "mpv_wakeup"              .}
+proc mpv_set_wakeup_callback*(ctx; cb: (pointer -> void); data)         {.importc: "mpv_set_wakeup_callback" .}
+proc mpv_wait_async_requests*(ctx)                                      {.importc: "mpv_wait_async_requests" .}
+proc mpv_hook_add*(ctx; udata; name; priority: cint): ErrorCode         {.importc: "mpv_hook_add"            .}
+proc mpv_hook_continue*(ctx; id: uint64): ErrorCode                     {.importc: "mpv_hook_continue"       .}
+
+proc mpv_render_context_create*(ren_ctx: ptr RenderContext; ctx; params: ptr RenderParam): ErrorCode {.importc: "mpv_render_context_create"             .}
+proc mpv_render_context_set_parameter*(ren_ctx; param: RenderParam): ErrorCode                       {.importc: "mpv_render_context_set_parameter"      .}
+proc mpv_render_context_get_info*(ren_ctx; param: RenderParam): ErrorCode                            {.importc: "mpv_render_context_get_info"           .}
+proc mpv_render_context_set_update_callback*(ren_ctx; cb: RenderUpdateFn; cb_ctx: pointer)           {.importc: "mpv_render_context_set_update_callback".}
+proc mpv_render_context_update*(ren_ctx): RenderUpdateFlag                                           {.importc: "mpv_render_context_update"             .}
+proc mpv_render_context_render*(ren_ctx; params: ptr RenderParam): ErrorCode                         {.importc: "mpv_render_context_render"             .}
+proc mpv_render_context_report_swap*(ren_ctx)                                                        {.importc: "mpv_render_context_report_swap"        .}
+proc mpv_render_context_free*(ren_ctx)                                                               {.importc: "mpv_render_context_free"               .}
+{.pop.}
+
+{.push inline.}
+
+template check_error(msg, body) =
+    let code = body
+    if code != Success:
+        raise new_exception(MPVError, "[MPV] " & msg & " : " & $(mpv_error_string code))
+
+func get_version*(): string =
+    let v = mpv_client_api_version()
+    try:
+        &"{(v shr 16) and 0xFFFF}.{v and 0xFFFF}"
+    except ValueError:
+        "<Unknown>"
+
+# proc mpv_set_property_string*(ctx; name; data: cstring): ErrorCode      {.importc: "mpv_set_property_string".}
+proc set_property*(ctx: Handle; name, data: string) {.raises: MPVError.} =
+    check_error "Failed to set property '" & name & "' to '" & data & "'":
+        ctx.mpv_set_property_string(cstring name, cstring data)
+
+proc client_name*(ctx): string = $(mpv_client_name ctx)
+proc client_id*(ctx):   int    = mpv_client_id ctx
+proc create*():         Handle = mpv_create()
+
+proc init*(ctx: Handle) {.raises: MPVError.} =
+    check_error "Failed to initialize":
+        mpv_initialize ctx
+
+proc destroy*(ctx: Handle) {.raises: MPVError.} =
+    check_error "Failed to destroy context":
+        mpv_destroy ctx
+
+proc set_wakeup_cb*(ctx: Handle; cb: pointer -> void; data: pointer = nil) =
+    ctx.mpv_set_wakeup_callback(cb, data)
+
+proc command_async*(ctx: Handle; args: varargs[string]) {.raises: MPVError.} =
+    let cargs = alloc_cstringarray args
+    check_error "Failed to submit async command":
+        ctx.mpv_command_async(0, cargs)
+
+    dealloc_cstringarray cargs
+
+iterator get_events*(ctx: Handle; timeout = 0.0): EventObj =
+    var result: ptr EventObj
+    while true:
+        result = ctx.mpv_wait_event timeout
+        if result.kind == None:
+            break
+
+        yield result[]
+
+# Rendering #
+
+proc create_render_context*(ctx: Handle; params: openArray[RenderParam]): RenderContext {.raises: MPVError.} =
+    check_error "Failed to create render context":
+        mpv_render_context_create(result.addr, ctx, params[0].addr)
+
+proc set_param*(ren_ctx; param: RenderParam) {.raises: MPVError.} =
+    check_error "Failed to set renderer parameter":
+        ren_ctx.mpv_render_context_set_parameter param
+
+proc update*(ren_ctx): RenderUpdateFlag =
+    ren_ctx.mpv_render_context_update
+
+proc render*(ren_ctx; params: varargs[RenderParam]) {.raises: MPVError.} =
+    let cparams = @params & RenderParam()
+    check_error "Failed to render with params":
+        ren_ctx.mpv_render_context_render cparams[0].addr
+
+proc set_update_cb*(ren_ctx; cb: RenderUpdateFn) =
+    ren_ctx.mpv_render_context_set_update_callback(cb, nil)
+
+proc report_swap*(ren_ctx) =
+    ren_ctx.mpv_render_context_report_swap
+
+proc destroy*(ren_ctx) =
+    mpv_render_context_free ren_ctx
+
 {.pop.}
